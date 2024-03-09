8163f4a2f2130dc7c5a1d5d9dc363c0ecf035492
ray <iissray57@gmail.com>
2024-03-09 20:16:08 +0900

    first Commit

---
+++ [A]bandage.py
@@ -0,0 +1,33 @@
+##[PCCP 기출문제] 1번 / 붕대 감기
+def solution(bandage, health, attacks):
+    answer = 0    
+    maxHealth = health
+    bandageTimer = 0
+    attackCount = 0
+    i = 1
+    lastAttackTime = attacks[-1][0]
+    
+    while i < lastAttackTime + 1:            
+        if attacks[attackCount][0] == i :
+            health -= attacks[attackCount][1] 
+            attackCount += 1
+            bandageTimer = 0
+        else :
+            if maxHealth > health :
+                health += bandage[1]
+                bandageTimer += 1
+            if bandageTimer == bandage[0] :
+                health += bandage[2]
+                bandageTimer = 0
+            if maxHealth < health :
+                health = maxHealth
+            
+        if health <= 0 :
+            answer = -1
+            break
+            
+        answer = health
+        i += 1    
+    
+    print(answer)
+    return answer
